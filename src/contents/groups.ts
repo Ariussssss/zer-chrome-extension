b76fa5d9d8c80908513daef36b6204e48334a8b1
Ariussssss <1059146811@qq.com>
2023-10-23 10:18:20 +0800

    chore: remove console

--- src/contents/groups.ts
+++ src/contents/groups.ts
@@ -1,6 +1,4 @@
 document.addEventListener('keydown', event => {
-  console.info("event", event);
-
   if (event.altKey && event.keyCode < 60 && event.keyCode > 48) {
     chrome.runtime.sendMessage({
       command: event.shiftKey ? 'moveToIndexGroup' : 'toggleIndex',
